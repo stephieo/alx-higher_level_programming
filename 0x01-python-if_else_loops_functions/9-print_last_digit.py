a4621ade887669cf6959c799e98df261f592fedd
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-31 14:30:17 +0000

    fixed print output: absolute value

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        last = number % -10
+        last = number % -(10)
     else:
         last = number % 10
-    print(last, end="")
+    print(abs(last), end="")
     return abs(last)
