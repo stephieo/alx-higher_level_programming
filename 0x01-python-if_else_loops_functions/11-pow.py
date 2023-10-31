d1e83084d5695fb5790ea9841d83d4de2a7be8ca
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-31 13:15:25 +0000

    tasks 10 and 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if a > 0 and b < 0:
+        result = 1 / (a ** (-b)) 
+    elif a < 0 and b > 0:
+        sign = -1 ** b
+        result = ((-a) ** b) * sign
+    else:
+        result = a ** b
+
+    return result
