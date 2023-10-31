1be59b1641dfac7979201609f5e41ef33316b38c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-31 14:27:36 +0000

    fixed name conflict

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if n < 0:
-        last = n % -10
+    if number < 0:
+        last = number % -10
     else:
-        last = n % 10
+        last = number % 10
     print(last, end="")
     return abs(last)
