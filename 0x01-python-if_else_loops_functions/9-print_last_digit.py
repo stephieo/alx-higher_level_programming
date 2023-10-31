9fe79ef0414441c458facbe1f63ff74644802bc9
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-31 14:17:16 +0000

    update to task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit, end="")
-    return last_digit
+        if n < 0:
+        last = n % -10
+    else:
+        last = n % 10
