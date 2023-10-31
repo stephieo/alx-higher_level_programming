b319417378bb5a1152da4fd4c3cd5751857012d3
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-31 14:43:46 +0000

    changed '&' to 'and

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if (i % 3) == 0 & (i % 5) == 0:
-            print("FizzBuzz", end=" ") 
+        if (i % 3) == 0 and (i % 5) == 0:
+            print("FizzBuzz", end=" ")
         if (i % 3) == 0:
             print("Fizz", end=" ")
         elif (i % 5) == 0 and i != 100:
