21cfae27526ac20093dc0cef68ae8fc84f360bb7
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-04 12:10:25 +0000

    task 6 output change

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -2,8 +2,18 @@
 
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 no_c = __import__('5-no_c').no_c
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 x = "chicago"
 print(no_c(x))
 listA = [2, 3, 4, 5, 6]
 print_reversed_list_integer(listA)
+
+matrix = [
+    [3, 5, 7],
+    [9, 11, 13],
+    [15, 17, 19]
+]
+print_matrix_integer(matrix)
+print('--')
+print_matrix_integer()
