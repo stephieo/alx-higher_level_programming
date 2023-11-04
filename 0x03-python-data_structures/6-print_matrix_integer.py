2ac26f763d2bfaee208e56ec103b6cd5d8a24668
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-04 13:05:06 +0000

    task 9

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-        for row in matrix:
-            for item in row:
-                if row.index(item) < (len(row) - 1):
-                    print("{:d}, ".format(item), end="")
-                else:
-                    print("{:d}".format(item), end="")
-            print()
+    for row in matrix:
+        for item in row:
+            if row.index(item) < (len(row) - 1):
+                print("{:d}, ".format(item), end="")
+            else:
+                print("{:d}".format(item), end="")
+        print()
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,12 @@
-  #!/usr/bin/python3
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-    last_num = len(my_list) - 1
     if not my_list:
         return
     else:
         maxnum = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > maxnum:
-                    maxnum = my_list[i]
+                maxnum = my_list[i]
             elif i == last_num:
                 if my_list[i] > maxnum:
                     maxnum = my_list[i]
