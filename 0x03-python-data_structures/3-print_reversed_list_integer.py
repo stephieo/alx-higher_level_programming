51fe0690d4af1f968596cc11cbb548d237db887b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-04 08:15:46 +0000

    task 3 - 5

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
+    for i in my_list:
+        print(i)
+
+
+alist = [1, 2, 3, 4]
+print_reversed_list_integer(alist)
