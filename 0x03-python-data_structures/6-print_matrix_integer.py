2510abbfa91f68071085fd6f5853dc68fe3ec81c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-04 12:02:36 +0000

    update to task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
             if row.index(item) < (len(row) - 1):
-                print("{}, ".format(item), end="")
+                print("{:d}, ".format(item), end="")
             else:
-                print("{}".format(item))
+                print("{:d}".format(item))
     print("--")
     print()
