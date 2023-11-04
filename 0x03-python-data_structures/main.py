8632c6e81d40d7cf9833071a0b0c6307b0e4ff41
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-04 11:40:03 +0000

    update: task 3

---
+++ 0x03-python-data_structures/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+listA = [2, 3, 4, 5, 6]
+print_reversed_list_integer(listA)
