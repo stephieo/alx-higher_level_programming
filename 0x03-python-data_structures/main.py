4ff3117595af97ed5b9ed86dfcae71b761e46487
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-04 11:54:26 +0000

    update to task 5

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+no_c = __import__('5-no_c').no_c
 
+x = "chicago"
+print(no_c(x))
 listA = [2, 3, 4, 5, 6]
 print_reversed_list_integer(listA)
