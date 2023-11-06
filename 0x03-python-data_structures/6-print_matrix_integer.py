1c206e965cff1001ed3b488eb6b6df2fbb9bfa3d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-06 10:05:18 +0000

    task 6: implemented logic for empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        return
     for row in matrix:
         for item in row:
             if row.index(item) < (len(row) - 1):
