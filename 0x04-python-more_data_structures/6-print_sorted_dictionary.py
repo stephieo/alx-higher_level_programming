c727106f77c8fc8152dc7a6cbb660800405a658d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-09 12:58:51 +0000

    fixed output: task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return sorted(a_dictionary)
+    sorted(a_dictionary)
+    in_order = a_dictionary.items()
+    for key, value in in_order:
+        print("{}: {}".format(key, value))
+    return in_order
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -2,6 +2,10 @@
 
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 search_replace = __import__('1-search_replace').search_replace
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
 
 my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
 new_list = search_replace(my_list, 2, 89)
