8f213aa34b9db28c311bef191c5bbb33c0ff264d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-09 09:10:04 +0000

    included shebang line

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    lambda: i ** i
+    square = lambda i: i ** i
     output = []
     for j in matrix:
-        output.append(list(map(lambda i: i ** i, matrix)))
+        output.append([square for i in j])
     print(output)
 
-test = [[2, 3, 4] , [2, 8, 9], [0, 5, 6]]
-square_matrix_simple(test)
---
+++ 0x04-python-more_data_structures/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+
+
+matrix = [
+    [1, 3, 2],
+    [7, 9, 2],
+    [3, 1, 6]
+]
+
+square_matrix_simple(matrix)
