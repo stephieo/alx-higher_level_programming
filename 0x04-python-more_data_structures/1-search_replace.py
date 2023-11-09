9d1e2411f9b104c6b5ff2c92f38c49ca756f32bb
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-09 11:36:34 +0000

    pycodestyle fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    if  my_list == []:
+    if my_list == []:
         return my_list
-    new_list = [i if i != search else replace for i in my_list ]
+    new_list = [i if i != search else replace for i in my_list]
     return new_list
