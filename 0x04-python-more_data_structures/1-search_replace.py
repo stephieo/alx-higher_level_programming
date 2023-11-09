3a4cbcb39120d0cae5532e0cea41a997a457f911
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-09 11:19:14 +0000

    updated conditiona;

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    if search >= len(new_list):
+    if search >= len(new_list) or my_list == []:
         return my_list
     new_list[search] = replace
     return new_list
