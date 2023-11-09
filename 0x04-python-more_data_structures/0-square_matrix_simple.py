e0aedc4ef1041c3bf2b3f42a46e3f2f282dc2cbe
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-09 10:36:02 +0000

    fixed pycodestyle: task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square = lambda i: i ** i
     output = []
     for j in matrix:
         output.append(list(map(lambda i: i ** 2, j)))
     return output
-
