c153ce77de2c4623a33f2b2f4c0c3ea5c685b998
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-10 09:47:14 +0000

    task 9

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -5,7 +5,12 @@ search_replace = __import__('1-search_replace').search_replace
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 update_dictionary = __import__('7-update_dictionary').update_dictionary
 simple_delete = __import__('8-simple_delete').simple_delete
+multiply_by_2 = __import__('9-multiply_by_2').multiply_by_2
 
+a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
+print_sorted_dictionary(a_dictionary)
+print("--")
 
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
 new_dict = simple_delete(a_dictionary, 'track')
