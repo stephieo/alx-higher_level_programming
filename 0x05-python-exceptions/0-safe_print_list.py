50f0a3448ed61e019d4c32c67551f286a7f9c579
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-20 11:30:16 +0000

    python exceptions: task 0 and 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    print_count = 0
+    try:
+        for i in my_list:
+            print("{:d}".format(i), end="")
+            print_count += 1
+    except IndexError as e:
+            print(e)
+        
+    print()
+    return print_count
