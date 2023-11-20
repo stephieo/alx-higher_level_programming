c3d05892f15f77343436344021c7f120ce992e1e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-20 13:50:06 +0000

    revert changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     print_count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
+    try:
+        for i in my_list:
+            print("{:d}".format(i), end="")
             print_count += 1
     except IndexError as e:
         break
