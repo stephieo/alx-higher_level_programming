c7861824e2eb5f0614441de0a5fccd17641ee9cd
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-20 21:28:18 +0000

    created: solution to task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    print_count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            print_count += 1
+        except ValueError:
+            continue
+    print()
+    return print_count
+
+list2 = [2, 3, 9, 5, ]
+print(safe_print_list_integers(list2, 56))
