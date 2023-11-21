278dcb385aa870d6755d3dee9f7cbbc98c5e289b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-21 13:33:47 +0000

    style: pycodestyle fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,17 +2,3 @@
 class Square:
     def __init__(self, squaresize=0):
         self.__size = squaresize
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-
