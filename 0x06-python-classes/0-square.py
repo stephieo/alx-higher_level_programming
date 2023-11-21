227d2816807a68b5b60654ef0460e2bfcb5dd8a2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-21 13:49:17 +0000

    doc: include docstrings for modules, classes and methods

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+""" Defines a square """
+
 class Square:
+    """class definition for Square. empty"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
+"""Defines a Square"""
 class Square:
+    """class definition of Square"""
     def __init__(self, squaresize=0):
+        """initializes instances of Square
+        
+        Args:
+            squaresize(int): length of one side of Square
+        """
         self.__size = squaresize
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-
