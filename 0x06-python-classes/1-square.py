5e468b3ae39772c4b635f47d97a453bb1c3c4d8b
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-21 13:54:50 +0000

    style: pycodestyle fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Defines a Square"""
+
+
 class Square:
     """class definition of Square"""
     def __init__(self, squaresize=0):
         """initializes instances of Square
-        
+
         Args:
             squaresize(int): length of one side of Square
         """
