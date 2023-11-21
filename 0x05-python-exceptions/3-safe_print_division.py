301588b097dd3e05566e084248ff3af75c8b3d40
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-21 10:14:26 +0000

    feat: solution to task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError as e:
+        result = None
+        return        
+    finally:
+        print("Inside result: {}".format(result))
