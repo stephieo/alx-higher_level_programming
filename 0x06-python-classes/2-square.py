aa7364c3c2682a611aa7b9c87fccdead46dbfdee
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-21 14:21:40 +0000

    feat: add solution to task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Square"""
+
+
+class Square:
+    """class definition of Square"""
+    def __init__(self, squaresize=0):
+        """initializes instances of Square
+
+        Args:
+            squaresize(int): length of one side of Square
+        """
+        if type(squaresize) is not int:
+            raise TypeError("size must be an integer")
+        if squaresize < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = squaresize
--- 0x06-python-classes/test.py
+++ 0x06-python-classes/test.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+Square = __import__('2-square').Square
 
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+    
 my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
