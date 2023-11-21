eda251f8172e6f7798238e5bd14ee380f01f42bc
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-21 10:16:11 +0000

    style: pycodestyle fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,6 @@ def safe_print_division(a, b):
         return result
     except ZeroDivisionError as e:
         result = None
-        return        
+        return
     finally:
         print("Inside result: {}".format(result))
