87bf9d8d038f96f9182734384217ed746fc7bd77
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-27 12:37:45 +0000

    feat: add solution to task 0 and test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This is the add_integer module
+
+contains only the add_integer() function
+
+>>>add_integer(4, 5)
+9
+"""
+import doctest
+
+def add_integer(a, b=98):
+    """adds two integers or floats
+
+    Args:
+        a (int): first number, can be a float too
+        b (int, optional): second number, can be a float. Defaults to 98
+    
+    Returns:
+        sum of the arguments
+    
+    Raises:
+        TypeError: if inputs are not int or float
+    """
+    if (type(a) is not int and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and type(b) is not float):
+        raise TypeError("b must be an integer")
+    
+    return (int(a) + int(b))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
