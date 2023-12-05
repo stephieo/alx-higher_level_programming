d46202c081aadc92045f9bc12ef1b04307df3f4a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-05 23:30:31 +0000

    docs: add docstring to method

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """defines the subclass `Rectangle`"""
     def __init__(self, width, height):
+        """initiallize object"""
         super().integer_validator("Rectangle", width)
         super().integer_validator("Rectangle", height)
         self.__width = width
