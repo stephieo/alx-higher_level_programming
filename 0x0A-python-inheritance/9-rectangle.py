0ed135ebdc746f489b82c7458b2f5d10b28e511f
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-05 23:35:29 +0000

    feat: add solution to task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""defines the subclass `Rectangle`"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines the subclass `Rectangle`"""
+    def __init__(self, width, height):
+        """initiallize object"""
+        super().integer_validator("Rectangle", width)
+        super().integer_validator("Rectangle", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
