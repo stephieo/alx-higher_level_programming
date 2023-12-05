9fe30f8af82dfd1aedb66f5e220bea6b26859fdf
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-05 23:06:55 +0000

    test: add test cases for task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,11 +4,18 @@
 
 class BaseGeometry:
     """class definition of BaseGeometry"""
+
     def area(self):
+        """raises an exception"""
         raise Exception("area() is not implemented")
 
-    def intege_validator(self, name, value):
+    def integer_validator(self, name, value):
+        """validates the input for integer_validator"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile('tests/7-base_geometry.txt')
