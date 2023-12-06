c4489f1170de0bf6ff4a1b7264255ccffa8a5a82
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-06 13:32:50 +0000

    fix: change variable name to be less conflicting

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,5 @@
 
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encdoding='utf-8') as file:
-        return file.append(text)
+    with open(filename, "a", encdoding='utf-8') as f:
+        return f.append(text)
