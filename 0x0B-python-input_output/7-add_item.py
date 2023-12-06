f06c0adee95f543007bc5cad9ff23fb39f2ee3ac
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-06 23:01:05 +0000

    feat: add solution to task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,11 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-allargs = []
 
-for item in sys.arg:
-    allargs.append(item)
+try:
+    allargs = load_from_json_file("add_item.json")
+except Exception:
+    allargs = []
 
+allargs.extend(sys.argv[1:])
 save_to_json_file(allargs, "add_item.json")
