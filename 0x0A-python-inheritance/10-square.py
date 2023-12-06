2ad09d7a629edfc0e5190db1f8db00edbb6a1fbe
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-06 00:30:29 +0000

    feat: add solution to task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" defines subclass of `Rectangle`, `Square`"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """definition of the subclass `Square`"""
+    def __init__(self, size):
+        """instantiation of square object"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """defines area of a square"""
+        return self.__size ** 2
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,13 +9,15 @@ class Rectangle(BaseGeometry):
     """defines the subclass `Rectangle`"""
     def __init__(self, width, height):
         """initiallize object"""
-        super().integer_validator("Rectangle", width)
-        super().integer_validator("Rectangle", height)
+        super().integer_validator("Rectangle width", width)
+        super().integer_validator("Rectangle height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """defines area of a rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
+        """defines user-friendly representation of object"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
