d7c17727c3a17f972a71776d1f1d542392b1b604
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-06 23:14:07 +0000

    fix: add return to function (task 6)

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """contains a function that creates an object from a json file"""
-
-
 import json
 
 
 def load_from_json_file(filename):
     """loads object from json file"""
     with open(filename, 'r') as x:
-        json.load(x)
+        return json.load(x)
