eebf1c0bb0b7f35a9fdbae1f334bbeddea05e2b7
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-12-06 21:51:07 +0000

    docs: improve function documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" defines a function that reads a text file to stdout"""
+"""defines a function that reads a text file to stdout"""
 
 
 def read_file(filename=""):
-    """opens a file and reads it"""
+    """opens a file and reads it
+    Args:
+        filename (str): name of file
+        """
     with open(filename, encoding='utf-8') as f:
         print(f.read())
 
