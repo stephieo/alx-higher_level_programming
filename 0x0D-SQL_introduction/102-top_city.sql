7ca8e63074fc3a3884adeb355c980fe49784efec
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-01-19 01:20:48 +0000

    fix: rename column

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- DISPLAYS AVG TEMP OF TOP THREE CITIES
-SELECT city, AVG(value) as avg FROM temperatures 
+SELECT city, AVG(value) as avg_temp FROM temperatures 
 WHERE month in (7, 8) GROUP BY city ORDER BY avg DESC LIMIT 3;
