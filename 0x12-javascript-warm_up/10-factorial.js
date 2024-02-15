ddf65a392ccdae18aa9f1fd22e9fb2538b0b528a
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-15 01:49:16 +0100

    feat: solutions to tasks 10 and 13

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const number = process.argv[2];
+
+function factorial (number) {
+  if (!number || number === 1) {
+    return 1;
+  } else {
+    return (factorial(number - 1) * number);
+  }
+}
+
+console.log(factorial(number));
