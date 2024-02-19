5d076d7e7c88f7c73181edf4efec64d23b612a26
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-19 15:11:05 +0100

    style: pycodestyle fix[tasks 8 and 9]

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,7 +3,7 @@
 SQLAlchemy query to return first state object/row from database
 """
 from sys import argv
-from  sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
@@ -11,10 +11,11 @@ MY_USER = argv[1]
 MY_PASS = argv[2]
 MY_DB = argv[3]
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     # create engine
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(MY_USER, MY_PASS, MY_DB),
+                           pool_pre_ping=True)
     # create session object and instance
     Session_factory = sessionmaker(bind=engine)
     session = Session_factory()
