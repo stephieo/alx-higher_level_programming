35b686221c3e5beba885897bd5d3ae6b6e1a913c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-19 14:59:46 +0100

    style: pycodestyle fix [task 11 - 12]

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,7 +3,7 @@
 Using Sqlalchemy to update a record in database
 """
 
-from sys import argv 
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -14,22 +14,25 @@ MY_DB = argv[3]
 
 if __name__ == "__main__":
     # create engine
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(MY_USER, MY_PASS, MY_DB),
+                           pool_pre_ping=True)
     # create session object and instance
     Session_factory = sessionmaker(bind=engine)
     session = Session_factory()
 
-    #query to get id of object/record to update
+    # query to get id of object/record to update
     result = session.query(State).filter(State.id == 2).first()
     print(result.name)
 
-    # 1.set object/record atribute to new value 2. add to database 3.commit changes
+    # 1.set object/record atribute to new value
+    # 2. add to database
+    # 3.commit changes
     result.name = "New Mexico"
     session.add(result)
     session.commit()
 
-    #check for successful commit
+    # check for successful commit
 
     print(result.id)
     print(result.name)
