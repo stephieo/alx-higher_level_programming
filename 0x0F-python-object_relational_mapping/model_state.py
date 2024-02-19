bb7674597110d3347eb65f9d7d70f2d5c6bb6452
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-19 15:16:55 +0100

    style: pycodestyle fix [model files]

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" This module  contains the class definition of a State class that maps to the rows in the States table"""
+""" This module  contains the class definition of a State class
+that maps to the rows in the States table
+"""
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-    
-#create base
+
+# create base
 Base = declarative_base()
 
-#create State class
+
+# create State class
 class State(Base):
     """definition of the class/model/table `State`"""
     __tablename__ = "states"
 
-    id = Column("id",Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column("name", String(128), nullable=False, )
-
-    
-
+    id = Column("id", Integer, autoincrement=True,
+                nullable=False, primary_key=True)
+    name = Column("name", String(128), nullable=False)
