240629acf767398e8c504ec686a9d1f242365762
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 17:23:37 +0100

    feat: [task 6] post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" This module sends a POST request to a given
+    URL and displays the body/text of the response
+    """
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+
+    r = requests.post(url, data={"email": email})
+
+    print(r.text)
