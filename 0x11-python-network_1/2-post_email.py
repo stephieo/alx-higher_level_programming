1cbd7488f4df337c3a08b86032fd01aa80865534
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 04:59:42 +0100

    feat: [task 2] using urllib to send POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,9 +3,22 @@
     URL and displays the value of a variable in the header
     """
 from sys import argv
-from urllib import request
+from urllib import request, parse
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
+    url = argv[1]
+    email = argv[2]
+
+    params = {"email": email}
+    querystring = parse.urlencode(params)
+    querystring = querystring.encode('ascii')
+    req = request.Request(url, data=querystring)
+
+
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(f"Your email is: {body.decode('UTF-8')}")
+
+
+
 
