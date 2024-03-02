5212b94874e1295031b981dbe39549888e7e3574
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 18:27:16 +0100

    feat: [task 7] fetch url and handle error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" This module fetches a url using urllib
+    and handles errors"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    r = requests.get(url)
+    if (r.ok):
+        print(r.text)
+    else:
+        print(f"Error code: {r.status_code}")
