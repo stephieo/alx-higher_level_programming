3b42d0ff624bae2e802c50f30c0b690a7ffa1ed3
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 17:15:27 +0100

    feat: fetch url using requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" This module fetches a url using the requests package """
+import requests
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print(f"\t- type: {type(r.text)}")
+    print(f"\t- content: {r.status_code}")
