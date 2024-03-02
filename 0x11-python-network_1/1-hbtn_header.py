2d265b5bd085a51f8b0acf2a8a2425a09599363d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 03:46:22 +0100

    feat: script to display value of a header in response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" This module sends a request to a given
+    URL and displays the value of a variable in the header
+    """
+from sys import argv
+from urllib import request
+
+if __name__ == "__main__":
+    url = argv[1]
+    with request.urlopen(url) as response:
+        body = response.read()
+        print(response.getheader(""))
+
