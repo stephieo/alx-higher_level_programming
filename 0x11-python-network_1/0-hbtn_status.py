f3abb0d70e8b2a24b903144c23810de4960cd971
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 02:11:49 +0100

    fix: [task 1] change output formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
 
-    print("""Body response:
-                - type: {}
-                - content: {}
-                - utf8 content: {}""".
-          format(type(body), body, body.decode("UTF-8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("UTF-8")))
