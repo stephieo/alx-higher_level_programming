9000e2431f513bd9cecd5e4e95f6af391b769c30
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-02 17:16:11 +0100

    feat: [task 5] fetch url and display variable in header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" This module sends a request to a given
+    URL and displays the value of a variable in the header
+    """
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+
+    print(r.headers.get('X-Request-Id'))
