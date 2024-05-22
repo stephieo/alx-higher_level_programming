93f7fb3d641a695c561ad0abb5eaf306fc4e9b6c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-05-22 15:06:29 +0100

    style:semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+exports.esrever = function (lst) {
+  const len = lst.length;
+  const revlst = [];
+  for (let ptr = len - 1; ptr > -1; ptr--) {
+    revlst.push(lst[ptr]);
+  }
+  return revlst;
+};
