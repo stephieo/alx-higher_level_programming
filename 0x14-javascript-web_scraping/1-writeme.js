2d05a8426153adab7b484b5234d9bd509c8b00e5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-05-24 13:52:18 +0100

    fix: correct arg

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const file = process.argv[2];
-const str = process.argv[2];
+const str = process.argv[3];
 
 fs.writeFile(file, str, 'utf-8', (error) => {
   if (error) {
