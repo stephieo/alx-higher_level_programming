0218ab2ea5fdffba901989e6db6752cd0e0d61bf
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-05-24 14:12:44 +0100

    feat: task 4 to 6

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url).on('response', (response) => {
-    console.log('code: ' + response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const args = process.argv;
+const request = require('request');
+const fs = require('fs');
+
+request(args[2], (err, code, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(args[3], body, 'utf-8', (err) => {
+      if (err) { console.error(err); }
+    });
+  }
+});
