015b03495d42c734683a599af1f64cbca9d4e088
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-05-24 13:53:50 +0100

    feat: task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, code, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
